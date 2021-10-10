255c8448c905f5c0cb0ebe4da83123739cb37c91
Raghav-Modani <raghavmodani4@gmail.com>
2021-10-10 09:26:42 +0530

    added the reandom 8-digit number to the API URL

--- pages/api/number.ts
+++ pages/api/number.ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import randomNumber from './utils/random-number'
 
 interface Data {
   number?: string;
@@ -7,7 +8,8 @@ interface Data {
 
 const number = (_req: NextApiRequest, res: NextApiResponse<Data>) => {
   if (_req.method === 'POST') {
-    res.status(200).json({ number: '001031' })
+    const number :String =  randomNumber(10).toString() +  randomNumber(10).toString() +  randomNumber(10).toString() +  randomNumber(10).toString() +  randomNumber(10).toString() +  randomNumber(10).toString() +  randomNumber(10).toString() +  randomNumber(10).toString() 
+    res.status(200).json({ number: number })
     return true
   }
   res.status(400).json({ message: 'Method Not Allowed' })
